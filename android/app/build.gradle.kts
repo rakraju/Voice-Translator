3f33a95c8d070f1953ac7f703f798054192bb7d1
Ramaraju <ramakrishnam.raj@gmail.com>
2026-08-15 13:39:14 +0530

    Change applicationId to match Play Console package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.aarkay.voicetranslator"
+        applicationId = "com.AARKAYSVoiceTranslator.myapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
